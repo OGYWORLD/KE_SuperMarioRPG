00b708703e5df1de735a276a0889dd7dab19606f
GHAWOORU <76478579+OGYWORLD@users.noreply.github.com>
2024-06-11 23:51:30 +0900

    Delete Assets/Assets directory

--- Assets/Assets/Scripts/Road/ScriptableObject/SceneNames.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObject/SceneNames", fileName = "SceneNames")]
-public class SceneNames : ScriptableObject
-{
-    public string sceneName;
-}
